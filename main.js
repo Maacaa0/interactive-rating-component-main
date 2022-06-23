fbe450e55f653cffba6fd9ac225026797f340ea2
Maacaa0 <102512313+Maacaa0@users.noreply.github.com>
2022-06-23 21:41:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 let targetDiv1 = document.querySelector(".main-page");
 let targetDiv2 = document.querySelector(".thankyou-page");
+let submitButton = document.querySelector(".submit");
+let elementClicked = false;
 
-    
-function submit() {
-    targetDiv1.style.display="none";
+
+submitButton.addEventListener('click',function submit() {
+    if (elementClicked == true) {
+    targetDiv1.style.display = "none";
     targetDiv2.style.display = "block";
+}  else {
+    alert("You need to select rating!")
 }
+});
+
+// JUST MY FUNCTION TO GET TO MAIN PAGE WITHOUT NEEDING TO REFRESH THE PAGE
 
 let backMainPage = document.querySelector(".thankyou-image");
 
 function back() {
     targetDiv1.style.display="block";
     targetDiv2.style.display = "none";
+    rating1.style.background = "var(--DarkBlue)";
+    rating1.style.color = "var(--LightGrey)";
+    rating2.style.background = "var(--DarkBlue)";
+    rating2.style.color = "var(--LightGrey)";
+    rating3.style.background = "var(--DarkBlue)";
+    rating3.style.color = "var(--LightGrey)";
+    rating4.style.background = "var(--DarkBlue)";
+    rating4.style.color = "var(--LightGrey)";
+    rating5.style.background = "var(--DarkBlue)";
+    rating5.style.color = "var(--LightGrey)";
+    elementClicked = false;
 }
+
+//-------------------------------------------------------------
 
 let replaceMe = document.querySelector(".replace")
 let rating1 = document.querySelector(".n1");
@@ -21,34 +42,85 @@ let rating3 = document.querySelector(".n3");
 let rating4 = document.querySelector(".n4");
 let rating5 = document.querySelector(".n5");
 
-rating1 = 1;
-rating2 = 2;
-rating3 = 3; 
-rating4 = 4;
-rating5 = 5;
 
-function select1() {
-    replaceMe.textContent = rating1;
+
+
+rating1.addEventListener('click', function() {
+    replaceMe.textContent = rating1.textContent;
+    elementClicked = true;
+    rating1.style.background = "var(--primary)";
+    rating1.style.color = "white";
+    rating2.style.background = "var(--DarkBlue)";
+    rating2.style.color = "var(--LightGrey)";
+    rating3.style.background = "var(--DarkBlue)";
+    rating3.style.color = "var(--LightGrey)";
+    rating4.style.background = "var(--DarkBlue)";
+    rating4.style.color = "var(--LightGrey)";
+    rating5.style.background = "var(--DarkBlue)";
+    rating5.style.color = "var(--LightGrey)";
+});
+
+
+rating2.addEventListener('click', function() {
+    replaceMe.textContent = rating2.textContent;
+    elementClicked = true;
+    rating2.style.background = "var(--primary)";
+    rating2.style.color = "white";
+    rating1.style.background = "var(--DarkBlue)";
+    rating1.style.color = "var(--LightGrey)";
+    rating3.style.background = "var(--DarkBlue)";
+    rating3.style.color = "var(--LightGrey)";
+    rating4.style.background = "var(--DarkBlue)";
+    rating4.style.color = "var(--LightGrey)";
+    rating5.style.background = "var(--DarkBlue)";
+    rating5.style.color = "var(--LightGrey)";
     
-}
+});
 
-function select2() {
-    replaceMe.textContent = rating2;
+rating3.addEventListener('click', function() {
+    replaceMe.textContent = rating3.textContent;
+    elementClicked = true;
+    rating3.style.background = "var(--primary)";
+    rating3.style.color = "white";
+    rating1.style.background = "var(--DarkBlue)";
+    rating1.style.color = "var(--LightGrey)";
+    rating2.style.background = "var(--DarkBlue)";
+    rating2.style.color = "var(--LightGrey)";
+    rating4.style.background = "var(--DarkBlue)";
+    rating4.style.color = "var(--LightGrey)";
+    rating5.style.background = "var(--DarkBlue)";
+    rating5.style.color = "var(--LightGrey)";
     
-}
+});
 
-function select3() {
-    replaceMe.textContent = rating3;
+rating4.addEventListener('click', function() {
+    replaceMe.textContent = rating4.textContent;
+    elementClicked = true;
+    rating4.style.background = "var(--primary)";
+    rating4.style.color = "white";
+    rating1.style.background = "var(--DarkBlue)";
+    rating1.style.color = "var(--LightGrey)";
+    rating3.style.background = "var(--DarkBlue)";
+    rating3.style.color = "var(--LightGrey)";
+    rating2.style.background = "var(--DarkBlue)";
+    rating2.style.color = "var(--LightGrey)";
+    rating5.style.background = "var(--DarkBlue)";
+    rating5.style.color = "var(--LightGrey)";
     
-}
+});
 
-function select4() {
-    replaceMe.textContent = rating4;
+rating5.addEventListener('click', function() {
+    replaceMe.textContent = rating5.textContent;
+    elementClicked = true;
+    rating5.style.background = "var(--primary)";
+    rating5.style.color = "white";
+    rating1.style.background = "var(--DarkBlue)";
+    rating1.style.color = "var(--LightGrey)";
+    rating3.style.background = "var(--DarkBlue)";
+    rating3.style.color = "var(--LightGrey)";
+    rating4.style.background = "var(--DarkBlue)";
+    rating4.style.color = "var(--LightGrey)";
+    rating2.style.background = "var(--DarkBlue)";
+    rating2.style.color = "var(--LightGrey)";
     
-}
-
-function select5() {
-    replaceMe.textContent = rating5;
-    
-}
-
+});
