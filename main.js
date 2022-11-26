e7dbab18d6ab1fb0d026379c435b709f3a7f5a7a
Maacaa0 <102512313+Maacaa0@users.noreply.github.com>
2022-11-26 20:51:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,126 +1,50 @@
 let targetDiv1 = document.querySelector(".main-page");
 let targetDiv2 = document.querySelector(".thankyou-page");
 let submitButton = document.querySelector(".submit");
-let elementClicked = false;
 
+const btns = document.querySelectorAll(".number-selector");
+let ratingValue = document.querySelector(".replace")
 
-submitButton.addEventListener('click',function submit() {
-    if (elementClicked == true) {
-    targetDiv1.style.display = "none";
-    targetDiv2.style.display = "block";
-}  else {
-    alert("You need to select rating!")
-}
-});
+btns.forEach(btn=>btn.addEventListener("click", function(e) {
+    ratingValue.textContent = this.textContent;
+    submitButton.disabled = false;
 
-// JUST MY FUNCTION TO GET TO MAIN PAGE WITHOUT NEEDING TO REFRESH THE PAGE
+    btns.forEach(x=> x.style.background = "var(--DarkBlue)");
+    btns.forEach(x=> x.style.color = "var(--LightGrey)")
 
-let backMainPage = document.querySelector(".thankyou-image");
+    e.target.style.background = "var(--primary)";
+    e.target.style.color = "white";
+}))
+
+submitButton.addEventListener("click", function() {
+    if (submitButton.disabled === false) {
+
+        targetDiv1.animate([
+            // keyframes
+            { transform: "scale(1)"},
+            { transform: 'scale(0)'}          
+            
+          ], {
+            // timing options
+            duration:400,
+            iterations: 1
+          });
+        setTimeout(() => {targetDiv1.style.display = "none"},400);
+        
+        setTimeout(() => {targetDiv2.style.display = "block"},500);
+        targetDiv2.animate([
+            // keyframes
+            { transform: "scale(0)"},
+            { transform: 'scale(1)'}
+          ], {
+            // timing options
+            duration: 700,
+            iterations: 1
+          });
+          
+    }
+})
 
 function back() {
-    targetDiv1.style.display="block";
-    targetDiv2.style.display = "none";
-    rating1.style.background = "var(--DarkBlue)";
-    rating1.style.color = "var(--LightGrey)";
-    rating2.style.background = "var(--DarkBlue)";
-    rating2.style.color = "var(--LightGrey)";
-    rating3.style.background = "var(--DarkBlue)";
-    rating3.style.color = "var(--LightGrey)";
-    rating4.style.background = "var(--DarkBlue)";
-    rating4.style.color = "var(--LightGrey)";
-    rating5.style.background = "var(--DarkBlue)";
-    rating5.style.color = "var(--LightGrey)";
-    elementClicked = false;
+    location.reload();
 }
-
-//-------------------------------------------------------------
-
-let replaceMe = document.querySelector(".replace")
-let rating1 = document.querySelector(".n1");
-let rating2 = document.querySelector(".n2");
-let rating3 = document.querySelector(".n3");
-let rating4 = document.querySelector(".n4");
-let rating5 = document.querySelector(".n5");
-
-
-
-
-rating1.addEventListener('click', function() {
-    replaceMe.textContent = rating1.textContent;
-    elementClicked = true;
-    rating1.style.background = "var(--primary)";
-    rating1.style.color = "white";
-    rating2.style.background = "var(--DarkBlue)";
-    rating2.style.color = "var(--LightGrey)";
-    rating3.style.background = "var(--DarkBlue)";
-    rating3.style.color = "var(--LightGrey)";
-    rating4.style.background = "var(--DarkBlue)";
-    rating4.style.color = "var(--LightGrey)";
-    rating5.style.background = "var(--DarkBlue)";
-    rating5.style.color = "var(--LightGrey)";
-});
-
-
-rating2.addEventListener('click', function() {
-    replaceMe.textContent = rating2.textContent;
-    elementClicked = true;
-    rating2.style.background = "var(--primary)";
-    rating2.style.color = "white";
-    rating1.style.background = "var(--DarkBlue)";
-    rating1.style.color = "var(--LightGrey)";
-    rating3.style.background = "var(--DarkBlue)";
-    rating3.style.color = "var(--LightGrey)";
-    rating4.style.background = "var(--DarkBlue)";
-    rating4.style.color = "var(--LightGrey)";
-    rating5.style.background = "var(--DarkBlue)";
-    rating5.style.color = "var(--LightGrey)";
-    
-});
-
-rating3.addEventListener('click', function() {
-    replaceMe.textContent = rating3.textContent;
-    elementClicked = true;
-    rating3.style.background = "var(--primary)";
-    rating3.style.color = "white";
-    rating1.style.background = "var(--DarkBlue)";
-    rating1.style.color = "var(--LightGrey)";
-    rating2.style.background = "var(--DarkBlue)";
-    rating2.style.color = "var(--LightGrey)";
-    rating4.style.background = "var(--DarkBlue)";
-    rating4.style.color = "var(--LightGrey)";
-    rating5.style.background = "var(--DarkBlue)";
-    rating5.style.color = "var(--LightGrey)";
-    
-});
-
-rating4.addEventListener('click', function() {
-    replaceMe.textContent = rating4.textContent;
-    elementClicked = true;
-    rating4.style.background = "var(--primary)";
-    rating4.style.color = "white";
-    rating1.style.background = "var(--DarkBlue)";
-    rating1.style.color = "var(--LightGrey)";
-    rating3.style.background = "var(--DarkBlue)";
-    rating3.style.color = "var(--LightGrey)";
-    rating2.style.background = "var(--DarkBlue)";
-    rating2.style.color = "var(--LightGrey)";
-    rating5.style.background = "var(--DarkBlue)";
-    rating5.style.color = "var(--LightGrey)";
-    
-});
-
-rating5.addEventListener('click', function() {
-    replaceMe.textContent = rating5.textContent;
-    elementClicked = true;
-    rating5.style.background = "var(--primary)";
-    rating5.style.color = "white";
-    rating1.style.background = "var(--DarkBlue)";
-    rating1.style.color = "var(--LightGrey)";
-    rating3.style.background = "var(--DarkBlue)";
-    rating3.style.color = "var(--LightGrey)";
-    rating4.style.background = "var(--DarkBlue)";
-    rating4.style.color = "var(--LightGrey)";
-    rating2.style.background = "var(--DarkBlue)";
-    rating2.style.color = "var(--LightGrey)";
-    
-});
